394485499a9dadd13f1fe9bbb6b16547d4a6fa6e
tamojuntoy477-pixel <tamojuntoy477@gmail.com>
2026-09-26 08:15:40 -0300

    NOX GBA: add nox-gba/app/build.gradle.kts

---
+++ nox-gba/app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.nox.gba"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.nox.gba"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
